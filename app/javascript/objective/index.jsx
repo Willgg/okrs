4736c067198d86ffbd80552d9c467ce5325f33d6
William Godard <grenier.godard@gmail.com>
2019-04-22 23:12:38 +0200

    add js files

---
+++ app/javascript/objective/index.jsx
@@ -0,0 +1,29 @@
+// external modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
+
+// internal modules
+import App from './components/app';
+import reducers from './reducers'
+
+const initialState = {
+  objectives: [],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
